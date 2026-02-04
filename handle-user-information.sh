f579ceb4a5f4181182f824566b94518efe3f0302
Ngnintedem3004 <ngnintedemmelong@gmail.com>
2026-02-04 17:45:54 +0100

    Add user information file

---
+++ handle-user-information.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Nom du fichier : $0"
+echo "Nombre d'arguments : $#"
+echo "Liste des arguments : $*"
+echo "Votre prénom: $1"
+echo "Votre job : $2"
+echo "Bonjour, $1! Vous êtes un(e) $2."
